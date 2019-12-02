3e5ae4a315edc06617bb1c7864af0297c78b1950
Christian Simmons <csimmonscs@gmail.com>
2019-12-02 11:08:23 -0800

    Uploading DoodleJumpClone Unity project to Github

---
+++ Doodle Jump/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.3f;
+
+    //private Vector3 currentVelocity;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(target.position.y > transform.position.y)
+        {
+
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            //transform.position = Vector3.SmoothDamp(transform.position, newPos,ref currentVelocity, smoothSpeed * Time.deltaTime);
+            transform.position = newPos;
+        }
+    }
+}
